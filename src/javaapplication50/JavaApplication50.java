db106d59ecedb052169ddbf0d35a0554a136ca3c
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-11-07 09:13:02 +0100

    A

---
+++ src/javaapplication50/JavaApplication50.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication50;
+
+/**
+ *
+ * @author ggarciamartinez
+ */
+public class JavaApplication50 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int  x = 0, n = 16 ;
+while (n % 2 == 0) {
+    x = x + n ;
+   n = n / 2 ;
+}
+System.out.println(x);
+    }
+    
+}
